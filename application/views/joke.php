7a973a4b729ebbbfe2508d75ee7a92c4d17f5ed3
hcxiong <cexo255@163.com>
2015-11-19 19:17:10 +0800

    no message

--- application/views/joke.php
+++ application/views/joke.php
@@ -3,8 +3,8 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta charset="utf-8">
-<title>猪猪笑话－搞笑段子、短信、无广告并且开源</title>
-<meta name="description" content="猪猪笑话－搞笑段子、短信、无广告并且开源">
+<title><?php if ($joke && $joke->title) { ?><?=$joke->title?> - <?php } ?>猪猪笑话－搞笑段子、短信、无广告并且开源</title>
+<meta name="description" content="<?php if ($joke) { ?><?=$joke->joke?><?php } else { ?>猪猪笑话－搞笑段子、短信、无广告并且开源<?php } ?>">
 <meta name="keywords" content="笑话,搞笑,段子,短信">
 </head>
 <body>
@@ -15,7 +15,7 @@
 	<h2>更多笑话</h2>
 	<ul>
 	<?php foreach($jokes as $info):?>
-		<li><a href="<?=BASEURI?>joke/<?=$info->jid?>"><h3><?=$info->title ? $info->title : mb_substr($info->joke, 0, 10,"utf-8").'...'?></h3></a><span><?=mb_substr($info->joke, 0, 30,"utf-8")?>...</span></li>
+		<li><a href="<?=BASEURI?>joke/<?=$info->jid?>" title="<?=$info->title ? $info->title : mb_substr($info->joke, 0, 10,"utf-8").'...'?>"><h3><?=$info->title ? $info->title : mb_substr($info->joke, 0, 10,"utf-8").'...'?></h3></a><p><?=mb_substr($info->joke, 0, 50,"utf-8")?>...</p></li>
 	<?php endforeach;?>
 	</ul>
 </body>
